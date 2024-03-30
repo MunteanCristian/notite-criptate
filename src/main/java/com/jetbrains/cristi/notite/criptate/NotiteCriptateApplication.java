e72b1829ae5b586f4cba11ff39758083914b852a
muntean cristian <munteancristian751@yahoo.com>
2024-03-30 21:18:50 +0200

    first commit

---
+++ src/main/java/com/jetbrains/cristi/notite/criptate/NotiteCriptateApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.cristi.notite.criptate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiteCriptateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotiteCriptateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbrains/cristi/notite/criptate/NotiteCriptateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.cristi.notite.criptate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotiteCriptateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
